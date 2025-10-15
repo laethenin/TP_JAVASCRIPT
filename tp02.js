1a66d79d140a01806e323b1ab0a5a2cb8c048131
laethenin <laetitiahenin@gmail.com>
2025-10-15 17:24:22 +0200

    ajout tp02

---
+++ tp02.js
@@ -0,0 +1,44 @@
+// déclaration des variables
+let num1 = 25;
+let num2 = 4;
+
+// réalisation des opérations mathématiques
+let addition = num1 + num2;
+let soustraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulo = num1 % num2;
+
+// affichage des résultats dans la console
+console.log (addition);
+console.log (soustraction);
+console.log (multiplication);
+console.log (division);
+console.log (modulo);
+
+// opérations de comparaison
+let plusGrand = num1 > num2;
+let plusPetit = num1 < num2;
+let egal = num1 === num2;
+let different = num1 !== num2;
+
+console.log (plusGrand);
+console.log (plusPetit);
+console.log (egal);
+console.log (different);
+
+// opérations avec booléens
+let b1 = true;
+let b2 = false;
+
+let and = b1&&b2;
+let or = b1 || b2;
+let not_b1 = !b1;
+let not_b2 = !b2;
+
+console.log (and);
+console.log (or);
+console.log (not_b1);
+console.log (not_b2);
+
+
